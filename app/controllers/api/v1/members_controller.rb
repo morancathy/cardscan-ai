e0a339ae9abe338409d534f455e0f4b609aa4853
Cat <cathy.moran@teladochealth.com>
2022-03-18 16:13:05 -0500

    create create method to make new members along with a 'strong params' to whitelist controller params

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -8,11 +8,29 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def create
+    # fetching all members relys on ActiveRecord to validate and save member details
+    # create method assigns all controller parmas provided into the model at once.
+    # Easy to create records, but opens possibility of malicious use. So we use 'strong parameters'
+    member = Member.create!(member_params)
+    if member
+      render json: member
+    else 
+      render json: member.errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  #strong parameters -
+    # params cant be assigned unless theyve been whitelisted. member_params is a private method where I whitelisted 
+    # controller params to prevent bad content from getting into my db.
+  private
+  
+  def member_params
+    params.permit(:user_id, :group_number, :payer_name)
   end
 end
