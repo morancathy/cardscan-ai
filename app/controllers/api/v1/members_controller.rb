603f8d03d6898a4d24d29c33d8557c9c4e1e6ba4
Cat <cathy.moran@teladochealth.com>
2022-03-18 16:28:55 -0500

    create show and destroy controller. Include private member method to find member by id

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,7 +1,11 @@
+# Rails uses ActiveRecord library to handle db related tasks. Connects classes to 
+# relational db tables and provides API for workign with them
+
 class Api::V1::MembersController < ApplicationController
-  def index
-    #all method provided by ActiveRecord, gets all members (ActiveRecord is the Schema)
-    #order method, orders them in descending order by created date, gives newest memebers first
+  def index    
+    # ActiveRecord querys the members table and fetches what exists in the db
+    # all method provided by ActiveRecord, gets all members (ActiveRecord is the Schema)
+    # order method, orders them in descending order by created date, gives newest memebers first
     # send list of members as JSON response with render
     member = Member.all.order(created_at: :desc)
     render json: member
@@ -20,9 +24,20 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def show
+    # check if member is returned by the member method and sent it as a JSON
+    # response, or send error if not.
+    if member
+      render json: member
+    else
+      render json: member.errors
+    end
   end
 
   def destroy
+    # & is safe navigaton operator. It avoids nil errors when calling a method.
+    # it deletes member only if it exits then send messge as response
+    member&.destroy
+    render json: {message: 'Member deleted!'}
   end
 
   #strong parameters -
@@ -32,5 +47,11 @@ class Api::V1::MembersController < ApplicationController
   
   def member_params
     params.permit(:user_id, :group_number, :payer_name)
+  end
+
+  def member
+    # uses ActiveRecord's find method to find a member whose id matches the id provided and 
+    # assigns it to an instance variable @member. 
+    @member ||=Member.find(params[:id])
   end
 end
