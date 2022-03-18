a496c53835e12a4ddc7c5c86829a7037fbfd32d4
Cat <cathy.moran@teladochealth.com>
2022-03-18 15:20:04 -0500

    update routes file iwth route for each action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      get 'members/index'
+      post 'members/create'
+      get '/show/:id', to: 'members#show'
+      delete '/destroy/:id', to: 'members#destroy'
+    end
+  end
   root 'homepage#index'
-  
-  get 'homepage/index'
+
+    # catch all route
+  get '/*path' => 'homepage#index'
+  # get 'homepage/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
