baf631d70117fcc82b4120384f49a66cc945582a
Cat <cathy.moran@teladochealth.com>
2022-03-18 14:36:37 -0500

    update file to be set up with api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+# class ApplicationController < ActionController::Base
+# end
+# commented out the above and added the below, to transer my app from
+# a full rails app to an API
+
+class ApplicationController < ActionController::API
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Adding this for api...this configures the format used when error occur in devlopmenet mode. 
+    config.debug_exception_response_format
+
 end
